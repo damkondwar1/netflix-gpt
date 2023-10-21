4ca01c8885ac829c881367cb2243d673adf542a5
unknown <krishnadamkondwar8@gmail.com>
2023-10-21 23:00:44 +0530

    SignIn SignUp header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 
 import './App.css';
+import Body  from './component/Body';
 
 function App() {
   return (
-    <div className="text-3xl font-bold text-green-800">
-     Namaste react netflix gpt
+    <div >
+     <Body/>
     </div>
   );
 }
 
 export default App;
+
+/* 
+planning
+   features
+   login/signup page 
+   signin/signup form 
+   redirect to signin
+   browsepage(after authentication)
+      -header
+      -main movie
+        trailer in background
+         title description
+           -movie suggestion
+             -movielist *n
+
+    -netflixgpt
+     -search bar
+     -movie suggestion 
+     
+
+*/
---
+++ src/component/Login.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import Header from './Header'
+import {useState} from "react"
+
+const Login = () => {
+  const[isSignInForm,setIsSignInForm]= useState(true)
+
+
+  const toggleSignInForm =()=>{
+        setIsSignInForm(!isSignInForm)
+  }
+
+
+  return (
+    <div>
+      <Header/>
+      <div className="absolute">
+      <img src="https://assets.nflxext.com/ffe/siteui/vlv3/a73c4363-1dcd-4719-b3b1-3725418fd91d/fe1147dd-78be-44aa-a0e5-2d2994305a13/IN-en-20231016-popsignuptwoweeks-perspective_alpha_website_large.jpg" 
+      alt="login"/>
+    </div>
+      <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+
+        <h1 className="font-bold text-3xl py-4">{isSignInForm ? "Sign In" : "Sign Up"}</h1>
+        
+        {!isSignInForm && (
+           <input type="text" placeholder="FullName" className="p-4 my-4 w-full bg-gray-500"/>
+        )}
+         
+        <input type="text" placeholder="Email address" className="p-4 my-4 w-full bg-gray-500"/>
+
+        <input type="password" placeholder="Password" className="p-4 my-4 w-full bg-gray-500 "/>
+
+        <button className="p-4 my-4 bg-red-700 w-full">{isSignInForm ? "Sign In" : "Sign Up"}</button>
+
+        <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>{isSignInForm ? "New To netflix Sign Up Now" : "Alreday Registered Sign In Now.."}</p>
+      </form>
+    </div>
+
+  )
+}
+
+export default Login
